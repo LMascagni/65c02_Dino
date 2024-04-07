beaf7364087db0808b7d365f684ecfa52de9923f
Mascagni Lorenzo <Mascagni.Lorenzo@isisbassafriulana.edu.it>
2024-04-07 16:20:21 +0200

    aggiunti commenti al codice e alle librerie

--- lib/bootstrap.s
+++ lib/bootstrap.s
@@ -1,39 +1,41 @@
-; I/O addresses 6000 -> 600F
+; Indirizzi I/O 6000 -> 600F
 PORTA   = $6001     
 PORTB   = $6000
 DDRA    = $6003
 DDRB    = $6002
-PCR     = $600c     ;   Peripherial Control Register -> 65c22
-IFR     = $600d     ;   Interrupt Flag Register -> 65c22
-IER     = $600e     ;   Interrupt Enable Register -> 65c22
+PCR     = $600c     ; Registro di controllo periferiche -> 65c22
+IFR     = $600d     ; Registro flag interrupt -> 65c22
+IER     = $600e     ; Registro abilitazione interrupt -> 65c22
 
-; Entrypoint for the program
+; Punto di ingresso per il programma
 reset:
-    ldx #$ff    ;   load $ff into X
-    txs         ;   transfer $ff as stack pointer
-    cli         ;   Clear Interrupt flag
+    ldx #$ff    ; Carica $ff in X
+    txs         ; Trasferisce $ff come puntatore stack
+    cli         ; Cancella il flag interrupt
 
-    lda #%10000010  ;   Enable CA1 interrupt
+    lda #%10000010  ; Abilita l'interrupt CA1
     sta IER
-    lda #$00        ;   Enable CA1 Negative Edge
+    lda #$00        ; Abilita il fronte negativo di CA1
     sta PCR
 
-    lda #%11111111  ;   Set 8 pins on PORTB to output
+    lda #%11111111  ; Imposta 8 pin su PORTB come output
     sta DDRB
 
-    lda #%11100000  ;   Set 3 pins on PORTA to output
+    lda #%11100000  ; Imposta 3 pin su PORTA come output
     sta DDRA
 
-    lda #%00111000  ;   8-bit - 2 line - 5x8 font (001<DL><N><F>xx)   
+    lda #%00111000  ; Display 8-bit - 2 linee - Font 5x8 (001<DL><N><F>xx)   
     jsr lcd_instruction
 
-    lda #%00001100  ;   Display on - Cursor off - Blink off 
+    lda #%00001100  ; Display acceso - Cursore spento - Lampeggio spento 
     jsr lcd_instruction
 
-    lda #%00000110  ;   Increment and shift cursor - don't shift display
+    lda #%00000110  ; Incremento e shift del cursore - non shifta il display
     jsr lcd_instruction
 
-    lda #$01  ;   Clear display
+    lda #$01  ; Pulisce il display
     jsr lcd_instruction
 
     jmp entrypoint
+
+
